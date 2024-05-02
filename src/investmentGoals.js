f1cb7110dd4f52de57bac78f346ea3935e2f6155
Maurício Soares <mauriciojunior2602@gmail.com>
2024-05-02 11:55:37 -0300

    formulario de investimento e arquivo de calculo de projeções criados

---
+++ src/investmentGoals.js
@@ -0,0 +1,58 @@
+function convertoMonthlyReturnRate(yearlyReturnRate) {
+  return yearlyReturnRate ** (1 / 12);
+}
+
+function generateReturnsArray(
+  startingAmount = 0,
+  timeHorizon = 0,
+  timePeriod = "monthly",
+  monthlyContribuition = 0,
+  returnRate = 0,
+  returnTimeFrame = "monthly"
+) {
+  if (!startingAmount || !timeHorizon) {
+    throw new Error(
+      "Investimento inicial e prazo devem ser preenchidos com valores positivos."
+    );
+  }
+
+  const finalReturnRate =
+    returnTimeFrame === "monthly"
+      ? 1 + returnRate / 100
+      : convertoMonthlyReturnRate(1 + returnRate / 100);
+
+  const finalTimeHorizon =
+    timePeriod === "monthly" ? timeHorizon : timeHorizon * 12;
+
+  const referenceInvestimentObject = {
+    investedAmount: startingAmount,
+    investReturns: 0,
+    totalInterestReturns: 0,
+    month: 0,
+    totalAmount: startingAmount,
+  };
+
+  const returnArray = [referenceInvestimentObject];
+  for (
+    let timeReference = 1;
+    timeReference <= finalTimeHorizon;
+    timeReference++
+  ) {
+    const totalAmount =
+      returnArray[timeReference - 1].totalAmount * finalReturnRate +
+      monthlyContribuition;
+
+      const investReturns = returnArray[timeReference - 1].totalAmount * finalReturnRate;
+      const investedAmount = startingAmount + monthlyContribuition * timeReference;
+      const totalInterestReturns = totalAmount - investedAmount;
+      returnArray.push({
+      investedAmount,
+      investReturns,
+      totalInterestReturns,
+      month: timeReference,
+      totalAmount,
+      });
+  }
+
+  return returnArray;
+}
